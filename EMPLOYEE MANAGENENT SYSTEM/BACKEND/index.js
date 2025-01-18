ca6f7388d525b0bfa6c60b811a2fb8004bdcd64e
GONGATI VYSHNAVI <155570715+vyshu0301@users.noreply.github.com>
2025-01-18 21:12:14 +0530

    Add files via upload

---
+++ EMPLOYEE MANAGENENT SYSTEM/BACKEND/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App.js'; // Ensure the extension is included
+import reportWebVitals from './reportWebVitals.js'; // Ensure the extension is included
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
